47efde150c84305dcddb68e0c1a0683d35f3a730
jsmolka <jsmolka@pm.me>
2021-04-27 22:34:34 +0200

    minor changes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -11,24 +11,15 @@ class Theme {
     return this.classList.contains("dark-mode");
   }
 
-  updateMetaColor(dark) {
-    const meta = document.querySelector('meta[name=theme-color]');
-
-    meta.setAttribute('content', dark ? '#24292E' : '#f5f5f5');
-  }
-
   init() {
     const theme = window.localStorage.getItem('theme');
-    const dark = this.classList.toggle('dark-mode', theme == null || theme === 'dark');
-
-    this.updateMetaColor(dark);
+    this.classList.toggle('dark-mode', theme == null || theme === 'dark');
   }
 
   toggle() {
     const dark = this.classList.toggle('dark-mode');
     window.localStorage.setItem('theme', dark ? 'dark' : 'light');
 
-    this.updateMetaColor(dark);
     this.onChanged(dark);
 
     return dark;
