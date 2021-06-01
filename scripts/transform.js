a77d5467a28208a84e395dbd11dd48561dc01ca2
jsmolka <jsmolka@pm.me>
2021-06-01 22:47:54 +0200

    optimize transform

--- scripts/transform.js
+++ scripts/transform.js
@@ -15,28 +15,10 @@ async function main() {
       const doc = dom.window.document;
 
       if (doc.getElementsByTagName('code').length > 0) {
-        const preloadMono = doc.createElement('link');
-        preloadMono.setAttribute('rel', 'preload');
-        preloadMono.setAttribute('href', '/font/mono/mono-400.woff2');
-        preloadMono.setAttribute('crossorigin', 'anonymous');
-        preloadMono.setAttribute('type', 'font/woff2');
-        preloadMono.setAttribute('as', 'font');
-        doc.head.appendChild(preloadMono);
-
         Prism.highlightAllUnder(doc);
-      }
 
-      if (doc.getElementsByClassName('font-bit').length > 0) {
-        const preloadBit = doc.createElement('link');
-        preloadBit.setAttribute('rel', 'preload');
-        preloadBit.setAttribute('href', '/font/bit/bit-outline.woff2');
-        preloadBit.setAttribute('crossorigin', 'anonymous');
-        preloadBit.setAttribute('type', 'font/woff2');
-        preloadBit.setAttribute('as', 'font');
-        doc.head.appendChild(preloadBit);
+        fs.writeFileSync(file, dom.serialize());
       }
-
-      fs.writeFileSync(file, dom.serialize());
     }
   });
 }
