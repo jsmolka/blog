500c5571092f143ef1133eeb9cdfe6bbe279aa5a
jsmolka <jsmolka@pm.me>
2021-06-13 22:00:06 +0200

    fully reimplement header

--- assets/scss/tailwind.config.js
+++ assets/scss/tailwind.config.js
@@ -14,6 +14,8 @@ function font(name) {
 }
 
 module.exports = {
+  darkMode: 'class',
+  important: true,
   theme: {
     colors: {
       current: 'currentColor',
@@ -28,17 +30,28 @@ module.exports = {
         'menu-hover': 'var(--menu-hover)',
         'accent': 'var(--accent)'
       },
-      complement: {
-        alpha: {
-          100: 'rgba(var(--complement), 0.1)',
-          200: 'rgba(var(--complement), 0.2)',
-          300: 'rgba(var(--complement), 0.3)',
-          400: 'rgba(var(--complement), 0.4)',
-          500: 'rgba(var(--complement), 0.5)',
-          600: 'rgba(var(--complement), 0.6)',
-          700: 'rgba(var(--complement), 0.7)',
-          800: 'rgba(var(--complement), 0.8)',
-          900: 'rgba(var(--complement), 0.9)'
+      alpha: {
+        black: {
+          10: 'rgba(0, 0, 0, 0.01)',
+          20: 'rgba(0, 0, 0, 0.02)',
+          30: 'rgba(0, 0, 0, 0.03)',
+          40: 'rgba(0, 0, 0, 0.04)',
+          50: 'rgba(0, 0, 0, 0.05)',
+          60: 'rgba(0, 0, 0, 0.06)',
+          70: 'rgba(0, 0, 0, 0.07)',
+          80: 'rgba(0, 0, 0, 0.08)',
+          90: 'rgba(0, 0, 0, 0.09)',
+        },
+        white: {
+          10: 'rgba(255, 255, 255, 0.01)',
+          20: 'rgba(255, 255, 255, 0.02)',
+          30: 'rgba(255, 255, 255, 0.03)',
+          40: 'rgba(255, 255, 255, 0.04)',
+          50: 'rgba(255, 255, 255, 0.05)',
+          60: 'rgba(255, 255, 255, 0.06)',
+          70: 'rgba(255, 255, 255, 0.07)',
+          80: 'rgba(255, 255, 255, 0.08)',
+          90: 'rgba(255, 255, 255, 0.09)',
         }
       }
     },
