5d0302e3dc624d8536a18a6fed8660aaaa4549bc
jsmolka <jsmolka@pm.me>
2021-06-16 22:49:28 +0200

    further improve things

--- assets/scss/tailwind.config.js
+++ assets/scss/tailwind.config.js
@@ -18,7 +18,6 @@ module.exports = {
   important: true,
   theme: {
     colors: {
-      black: 'black',
       current: 'currentColor',
       transparent: 'transparent',
       var: {
@@ -31,28 +30,10 @@ module.exports = {
         'menu-hover': 'var(--menu-hover)',
         'accent': 'var(--accent)'
       },
-      alpha: {
-        black: {
-          10: 'rgba(0, 0, 0, 0.01)',
-          20: 'rgba(0, 0, 0, 0.02)',
-          30: 'rgba(0, 0, 0, 0.03)',
-          40: 'rgba(0, 0, 0, 0.04)',
-          50: 'rgba(0, 0, 0, 0.05)',
-          60: 'rgba(0, 0, 0, 0.06)',
-          70: 'rgba(0, 0, 0, 0.07)',
-          80: 'rgba(0, 0, 0, 0.08)',
-          90: 'rgba(0, 0, 0, 0.09)',
-        },
-        white: {
-          10: 'rgba(255, 255, 255, 0.01)',
-          20: 'rgba(255, 255, 255, 0.02)',
-          30: 'rgba(255, 255, 255, 0.03)',
-          40: 'rgba(255, 255, 255, 0.04)',
-          50: 'rgba(255, 255, 255, 0.05)',
-          60: 'rgba(255, 255, 255, 0.06)',
-          70: 'rgba(255, 255, 255, 0.07)',
-          80: 'rgba(255, 255, 255, 0.08)',
-          90: 'rgba(255, 255, 255, 0.09)',
+      hover: {
+        alpha: {
+          light: 'rgba(0, 0, 0, 0.02)',
+          dark: 'rgba(255, 255, 255, 0.04)'
         }
       }
     },
