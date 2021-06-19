300fac20ea62c549beb14ebdf9c4666779ec3af9
jsmolka <jsmolka@pm.me>
2021-06-19 21:19:07 +0200

    make eggvance wasm work again

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,13 +1,32 @@
-const theme = localStorage.getItem('theme');
-const themeButtons = document.getElementsByClassName('theme-button');
+class Theme {
+  constructor() {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      document.body.classList.toggle('dark', theme === 'dark');
+    }
 
-if (theme) {
-  document.body.classList.toggle('dark', theme === 'dark');
+    this.onChanged = dark => {};
+  }
+
+  get isDark() {
+    return document.body.classList.contains('dark');
+  }
+
+  toggle() {
+    const dark = document.body.classList.toggle('dark');
+    localStorage.setItem('theme', dark ? 'dark' : 'light');
+    this.onChanged(dark);
+
+    return dark;
+  }
 }
+
+window.theme = new Theme();
+
+const themeButtons = document.getElementsByClassName('theme-button');
 
 for (const themeButton of themeButtons) {
   themeButton.addEventListener('click', () => {
-    const dark = document.body.classList.toggle('dark');
-    localStorage.setItem('theme', dark ? 'dark' : 'light');
+    window.theme.toggle();
   });
 }
