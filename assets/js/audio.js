5c8d3a9504867067a6d445d64c8e697da8358a34
jsmolka <jsmolka@pm.me>
2021-07-13 17:57:58 +0200

    use string interpolation for audio template

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -35,6 +35,13 @@ class BarEventHandler {
   }
 }
 
+const icons = {
+  play: 'M8 5.14v14l11-7l-11-7z',
+  pause: 'M14 19h4V5h-4M6 19h4V5H6v14z',
+  muted: 'M12 4L9.91 6.09L12 8.18M4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.26c-.67.51-1.42.93-2.25 1.17v2.07c1.38-.32 2.63-.95 3.68-1.81L19.73 21L21 19.73l-9-9M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.916 8.916 0 0 0 21 12c0-4.28-3-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71m-2.5 0c0-1.77-1-3.29-2.5-4.03v2.21l2.45 2.45c.05-.2.05-.42.05-.63z',
+  volume: 'M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.84-5 6.7v2.07c4-.91 7-4.49 7-8.77c0-4.28-3-7.86-7-8.77M16.5 12c0-1.77-1-3.29-2.5-4.03V16c1.5-.71 2.5-2.24 2.5-4M3 9v6h4l5 5V4L7 9H3z'
+};
+
 export default class AudioPlayer {
   static instances = [];
 
@@ -252,21 +259,21 @@ export default class AudioPlayer {
 
   updatePlayPauseIcon() {
     this.playPause.buttonPath.attributes.d.value = this.audio.paused
-      ? 'M8 5.14v14l11-7l-11-7z'
-      : 'M14 19h4V5h-4M6 19h4V5H6v14z';
+      ? icons.play
+      : icons.pause;
   }
 
   updateVolumeIcon() {
     this.volume.buttonPath.attributes.d.value = this.audio.muted || this.audio.volume === 0
-      ? 'M12 4L9.91 6.09L12 8.18M4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.26c-.67.51-1.42.93-2.25 1.17v2.07c1.38-.32 2.63-.95 3.68-1.81L19.73 21L21 19.73l-9-9M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.916 8.916 0 0 0 21 12c0-4.28-3-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71m-2.5 0c0-1.77-1-3.29-2.5-4.03v2.21l2.45 2.45c.05-.2.05-.42.05-.63z'
-      : 'M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.84-5 6.7v2.07c4-.91 7-4.49 7-8.77c0-4.28-3-7.86-7-8.77M16.5 12c0-1.77-1-3.29-2.5-4.03V16c1.5-.71 2.5-2.24 2.5-4M3 9v6h4l5 5V4L7 9H3z';
+      ? icons.muted
+      : icons.volume;
   }
 
   get template() {
     return /* html */ `
       <div class="flex items-center pl-2 pr-2.5 py-2 bg-var-background-secondary rounded-sm shadow-sm">
         <svg class="audio-play-pause-button mb-px text-var-audio hover:text-var-audio-secondary select-none cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-          <path class="audio-play-pause-button-path" d="M8 5.14v14l11-7l-11-7z" />
+          <path class="audio-play-pause-button-path" d="${icons.play}" />
         </svg>
         <div class="audio-time ml-2 text-sm text-var-audio-secondary">0:00 / 0:00</div>
         <div class="audio-progress-bar flex flex-1 ml-3.5 mr-3 py-2 cursor-pointer">
@@ -281,7 +288,7 @@ export default class AudioPlayer {
             </div>
           </div>
           <svg class="audio-volume-button text-var-audio hover:text-var-audio-secondary select-none cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-            <path class="audio-volume-button-path" d="M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.84-5 6.7v2.07c4-.91 7-4.49 7-8.77c0-4.28-3-7.86-7-8.77M16.5 12c0-1.77-1-3.29-2.5-4.03V16c1.5-.71 2.5-2.24 2.5-4M3 9v6h4l5 5V4L7 9H3z" />
+            <path class="audio-volume-button-path" d="${icons.volume}" />
           </svg>
         </div>
       </div>
