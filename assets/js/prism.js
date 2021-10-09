2b34f86a069692a9e9ce4025833442512d3d5528
jsmolka <jsmolka@pm.me>
2021-10-09 17:41:45 +0200

    add signs of life post

--- assets/js/prism.js
+++ assets/js/prism.js
@@ -19,6 +19,35 @@ Prism.languages.armv4t = {
   number: /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
 };
 
+Prism.languages.drizzle = {
+	comment: {
+		pattern: /(^|[^\\])#.*/,
+		lookbehind: true
+	},
+	'triple-quoted-string': {
+		pattern: /(?:[rub]|br|rb)?(""")[\s\S]*?\1/i,
+		greedy: true,
+		alias: 'string'
+	},
+	string: {
+		pattern: /(?:[rub]|br|rb)?(")(?:\\.|(?!\1)[^\\\r\n])*\1/i,
+		greedy: true
+	},
+	function: {
+		pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_]\w*(?=\s*\()/g,
+		lookbehind: true
+	},
+	'class-name': {
+		pattern: /(\bclass\s+)\w+/i,
+		lookbehind: true
+	},
+	keyword: /\b(assert|block|break|case|class|continue|def|elif|else|extends|false|for|if|in|noop|null|print|return|super|switch|this|true|var|while|yield)\b/,
+	boolean: /\b(?:false|null|true)\b/,
+	number: /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
+	operator: /[-+%=]=?|!=|:=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
+	punctuation: /[{}[\];(),.:]/
+};
+
 Prism.languages.cpp.keyword = [
   Prism.languages.cpp.keyword,
   /\b(u8|u16|u32|u64|s8|s16|s32|s64|uint)\b/,
