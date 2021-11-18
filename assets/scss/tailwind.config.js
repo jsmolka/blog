2d0b6d2da10c2aa99b093f15885a0ea36677930c
jsmolka <jsmolka@pm.me>
2021-11-18 19:35:14 +0100

    add alliance font

--- assets/scss/tailwind.config.js
+++ assets/scss/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       meta: 'var(--meta)',
     },
     fontFamily: {
-      roboto: 'roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+      alliance: 'alliance, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
       mono: 'mono, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
     screens: {
