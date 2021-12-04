a21a754630bba41a242291d43a9b1475bdd05d01
jsmolka <jsmolka@pm.me>
2021-12-04 09:45:31 +0100

    add editorconfig

--- assets/js/prism.js
+++ assets/js/prism.js
@@ -20,32 +20,32 @@ Prism.languages.armv4t = {
 };
 
 Prism.languages.drizzle = {
-	comment: {
-		pattern: /(^|[^\\])#.*/,
-		lookbehind: true
-	},
-	'triple-quoted-string': {
-		pattern: /(?:[rub]|br|rb)?(""")[\s\S]*?\1/i,
-		greedy: true,
-		alias: 'string'
-	},
-	string: {
-		pattern: /(?:[rub]|br|rb)?(")(?:\\.|(?!\1)[^\\\r\n])*\1/i,
-		greedy: true
-	},
-	function: {
-		pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_]\w*(?=\s*\()/g,
-		lookbehind: true
-	},
-	'class-name': {
-		pattern: /(\bclass\s+)\w+/i,
-		lookbehind: true
-	},
-	keyword: /\b(assert|block|break|case|class|continue|def|elif|else|extends|false|for|if|in|noop|null|print|return|super|switch|this|true|var|while|yield)\b/,
-	boolean: /\b(?:false|null|true)\b/,
-	number: /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
-	operator: /[-+%=]=?|!=|:=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
-	punctuation: /[{}[\];(),.:]/
+  comment: {
+    pattern: /(^|[^\\])#.*/,
+    lookbehind: true
+  },
+  'triple-quoted-string': {
+    pattern: /(?:[rub]|br|rb)?(""")[\s\S]*?\1/i,
+    greedy: true,
+    alias: 'string'
+  },
+  string: {
+    pattern: /(?:[rub]|br|rb)?(")(?:\\.|(?!\1)[^\\\r\n])*\1/i,
+    greedy: true
+  },
+  function: {
+    pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_]\w*(?=\s*\()/g,
+    lookbehind: true
+  },
+  'class-name': {
+    pattern: /(\bclass\s+)\w+/i,
+    lookbehind: true
+  },
+  keyword: /\b(assert|block|break|case|class|continue|def|elif|else|extends|false|for|if|in|noop|null|print|return|super|switch|this|true|var|while|yield)\b/,
+  boolean: /\b(?:false|null|true)\b/,
+  number: /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
+  operator: /[-+%=]=?|!=|:=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
+  punctuation: /[{}[\];(),.:]/
 };
 
 Prism.languages.cpp.keyword = [
