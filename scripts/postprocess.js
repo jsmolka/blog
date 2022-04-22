7955f650a6d7430eead69f04899bb7bd70c66e27
jsmolka <jsmolka@pm.me>
2022-04-22 19:12:43 +0200

    better handle inheritance in postprocess

--- scripts/postprocess.js
+++ scripts/postprocess.js
@@ -4,16 +4,14 @@ const glob = require('glob');
 const { JSDOM } = require('jsdom');
 require('../assets/js/prism');
 
-const root = path.join(__dirname, '../public/').replace(/\\/g, '/');
-const hrefWeb = `href="/`;
-const hrefFile = `href="file://${root}`;
+const public = path.join(__dirname, '../public/').replace(/\\/g, '/');
+const hrefRel = `href="/`;
+const hrefAbs = `href="file://${public}`;
 
 function load(file) {
   let html = fs.readFileSync(file).toString();
-  html = html.replace(new RegExp(hrefWeb, 'g'), hrefFile);
-  const jsdom = new JSDOM(html, {
-    resources: 'usable'
-  });
+  html = html.replace(new RegExp(hrefRel, 'g'), hrefAbs);
+  const jsdom = new JSDOM(html, { resources: 'usable' });
   return new Promise((resolve) => {
     jsdom.window.onload = () => {
       resolve(jsdom);
@@ -22,7 +20,7 @@ function load(file) {
 }
 
 function save(file, html) {
-  html = html.replace(new RegExp(hrefFile, 'g'), hrefWeb);
+  html = html.replace(new RegExp(hrefAbs, 'g'), hrefRel);
   fs.writeFileSync(file, html);
 }
 
@@ -38,53 +36,54 @@ function highlight(window) {
 function preload(window) {
   const document = window.document;
 
-  const analyze = element => {
+  const analyze = (element) => {
     const computed = window.getComputedStyle(element);
     return [
       computed.getPropertyValue('font-family').split(',')[0],
-      computed.getPropertyValue('font-weight') || 400,
-      computed.getPropertyValue('font-style') || 'normal'
+      computed.getPropertyValue('font-weight'),
+      computed.getPropertyValue('font-style')
     ];
   };
 
-  const html = document.documentElement;
-  const parent = analyze(html);
-  if (!parent[0]) {
-    return false;
-  }
-
-  const analyzeChild = element => {
-    const data = analyze(element);
-    const inherit = (value, parentValue) => !value || value === 'inherit' ? parentValue : value;
-    return [
-      inherit(data[0], parent[0]),
-      inherit(data[1], parent[1]),
-      inherit(data[2], parent[2])
-    ];
-  };
+  const stack = [];
+  stack.push(analyze(document.documentElement));
+  stack[0][0] = stack[0][0] || 'sans-serif';
+  stack[0][1] = stack[0][1] || 400;
+  stack[0][2] = stack[0][2] || 'normal';
 
   const fonts = new Set();
-  const addFont = data => {
+  const traverse = (element) => {
+    const data = analyze(element);
+    for (const [index, inherit] of stack[stack.length - 1].entries()) {
+      if (!data[index] || data[index] === 'inherit') {
+        data[index] = inherit;
+      }
+    }
+    stack.push(data);
+    for (const child of element.childNodes) {
+      if (child instanceof window.Element) {
+        traverse(child);
+      }
+    }
+    stack.pop();
+
     data[0] = data[0].toLowerCase().replace(/ /g, '-');
     if (data[2] !== 'italic') {
       data.pop();
     }
     fonts.add(data.join('-'));
-  };
-
-  addFont(parent);
-  for (const element of document.getElementsByTagName('*')) {
-    addFont(analyzeChild(element));
   }
+
+  traverse(document.getElementsByTagName('body')[0]);
 
   let changed = false;
   for (const font of fonts) {
     const href = `/fonts/${font}.woff2`;
-    const file = path.join(root, href);
+    const file = path.join(public, href);
     if (fs.existsSync(file)) {
       const link = document.createElement('link');
-      link.href = href;
-      link.rel = 'preload';
+      link.setAttribute('href', href);
+      link.setAttribute('rel', 'preload');
       link.setAttribute('as', 'font');
       link.setAttribute('type', 'font/woff2');
       link.setAttribute('crossorigin', '');
@@ -98,7 +97,7 @@ function preload(window) {
 function trim(window) {
   let changed = false;
   const document = window.document;
-  const isInternalLink = href => href.match(/^(https:\/\/smolka\.dev)?\/.*\/$/);
+  const isInternalLink = (href) => href.match(/^(https:\/\/smolka\.dev)?\/.*\/$/);
   for (const link of document.getElementsByTagName('a')) {
     const href = link.getAttribute('href');
     if (isInternalLink(href)) {
@@ -110,7 +109,7 @@ function trim(window) {
 }
 
 function main() {
-  glob(root + '**/*.html', async (_, files) => {
+  glob(public + '**/*.html', async (_, files) => {
     for (const file of files) {
       const jsdom = await load(file);
       const window = jsdom.window;
