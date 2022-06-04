d32f678b51ab770499723b72060ccb69ad256470
jsmolka <jsmolka@pm.me>
2022-06-04 09:38:53 +0200

    improve subsets

--- scripts/subset.sh
+++ scripts/subset.sh
@@ -1,4 +1,4 @@
-pyftsubset Inter.ttf --unicodes="U+0000-017F,U+2192,U+2190,U+21A9" --layout-features="cv07" --flavor="woff2" --output-file="inter.woff2"
+pyftsubset Inter.ttf --unicodes="U+0000-017F,U+2192,U+2190,U+21A9" --layout-features="calt,ccmp,kern,cv07,tnum" --flavor="woff2" --output-file="inter.woff2"
 pyftsubset JetBrainsMono.ttf --unicodes="U+0000-017F" --layout-features="" --flavor="woff2" --output-file="jetbrains-mono.woff2"
 pyftsubset JetBrainsMono-Italic.ttf --unicodes="U+0000-017F" --layout-features="" --flavor="woff2" --output-file="jetbrains-mono-italic.woff2"
 
