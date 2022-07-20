cf57737380f1a3931be2644251ce52e799e9dc7c
jsmolka <jsmolka@pm.me>
2022-07-20 22:46:05 +0200

    more consistent image sizing

---
+++ scripts/resize-images.sh
@@ -0,0 +1,5 @@
+# sudo apt-get install imagemagick
+
+mogrify -resize 2048x2048\> *.png
+mogrify -resize 2048x2048\> -format jpg *.jpg
+mogrify -resize 2048x2048\> -format jpg *.jpeg
--- scripts/subset.sh
+++ scripts/subset.sh
@@ -1,3 +1,5 @@
+# sudo apt-get install fonttools
+
 pyftsubset Inter.ttf --unicodes="U+0000-017F,U+2000-201F,U+20AC,U+2190,U+2192,U+21A9" --layout-features="calt,ccmp,kern,cv07,tnum" --flavor="woff2" --output-file="../static/fonts/inter.woff2"
 pyftsubset JetBrainsMono.ttf --unicodes="U+0000-017F" --layout-features="" --flavor="woff2" --output-file="../static/fonts/jetbrains-mono.woff2"
 pyftsubset JetBrainsMono-Italic.ttf --unicodes="U+0000-017F" --layout-features="" --flavor="woff2" --output-file="../static/fonts/jetbrains-mono-italic.woff2"
