4619d110b2f47f6a61f0c0a9d7f9c07853b11b42
jsmolka <jsmolka@pm.me>
2022-10-01 21:27:45 +0200

    simplify app creation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
 import { createApp, reactive } from 'petite-vue';
 import Audio from './audio';
 
-for (const element of document.querySelectorAll('[v-scope*="Audio"]')) {
-  createApp({ Audio }).mount(element);
-}
+const store = reactive({ menu: false });
 
-const store = reactive({
-  menu: false,
-});
-
-createApp({ store }).mount();
+createApp({ Audio, store }).mount();
 
 for (const type of ['click', 'resize']) {
   window.addEventListener(type, () => store.menu = false);
