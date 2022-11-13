c6c2e168f94c63fa76530667e7e2d4e2330f4c28
jsmolka <jsmolka@pm.me>
2022-11-13 14:23:16 +0100

    improve theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,29 +1,40 @@
 const html = document.documentElement;
+const dark = window.matchMedia('(prefers-color-scheme: dark)');
 
 class Theme {
   constructor() {
     this.callbacks = [];
-    this.set(localStorage.getItem('theme') ?? 'system');
+    this.set(this.get());
+
+    dark.addEventListener('change', ({ matches }) => {
+      if (this.get() === 'system') {
+        this.update(matches);
+      }
+    });
   }
 
   get isDark() {
-    switch (html.getAttribute('theme')) {
+    switch (this.get()) {
       case 'system':
-        return window.matchMedia('(prefers-color-scheme: dark)').matches;
+        return dark.matches;
       case 'dark':
         return true;
-      case 'light':
-        return false;
-      default:
-        return false;
-    }
+      }
+      return false;
+  }
+
+  get() {
+    return localStorage.getItem('theme') ?? 'system';
   }
 
   set(theme) {
     html.setAttribute('theme', theme);
     localStorage.setItem('theme', theme);
+    this.update(this.isDark);
+  }
 
-    const dark = html.classList.toggle('dark', this.isDark);
+  update(dark) {
+    html.classList.toggle('dark', dark);
     for (const callback of this.callbacks) {
       callback(dark);
     }
