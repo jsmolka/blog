45acf9bb4a274dc644ca685520b22388e1ceba6c
jsmolka <jsmolka@pm.me>
2022-11-20 17:24:06 +0100

    improve theme button and header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 import { createApp, reactive } from 'petite-vue';
 import Audio from './audio';
 
-const store = reactive({ menu: false });
+const store = reactive({
+  get theme() {
+    return window.theme.value;
+  },
+  set theme(value) {
+    window.theme.value = value;
+  },
+ });
 
 createApp({ Audio, store }).mount();
-
-for (const type of ['click', 'resize']) {
-  window.addEventListener(type, () => store.menu = false);
-}
