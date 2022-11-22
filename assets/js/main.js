8d92969e35c07aa84c0e8c862dc1634f8a6ea71b
jsmolka <jsmolka@pm.me>
2022-11-22 17:26:32 +0100

    add mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ import { createApp, reactive } from 'petite-vue';
 import Audio from './audio';
 
 const store = reactive({
+  menu: false,
   get theme() {
     return window.theme.value;
   },
@@ -11,3 +12,7 @@ const store = reactive({
  });
 
 createApp({ Audio, store }).mount();
+
+for (const type of ['click', 'resize']) {
+  window.addEventListener(type, () => store.menu = false);
+}
