bb9352ca2511f2ec55171e32a6d25ff9192b2c42
jsmolka <jsmolka@pm.me>
2023-03-01 22:50:30 +0100

    improve subset

--- scripts/subset.sh
+++ scripts/subset.sh
@@ -1,5 +1,13 @@
 # sudo apt-get install fonttools
 
-pyftsubset Inter.ttf --unicodes="U+0000-017F,U+2000-201F,U+20AC,U+2190,U+2192,U+21A9" --layout-features="calt,ccmp,kern,cv07,tnum" --flavor="woff2" --output-file="../assets/fonts/inter.woff2"
-pyftsubset JetBrainsMono.ttf --unicodes="U+0000-017F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono.woff2"
-pyftsubset JetBrainsMono-Italic.ttf --unicodes="U+0000-017F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono-italic.woff2"
+# https://jrgraphix.net/r/Unicode/
+# U+0000-007F  Latin
+# U+0080-00FF  Latin-1 Supplement
+# U+2010-201F  Dashes
+# U+20AC       €
+# U+2190       ←
+# U+2192       →
+# U+21A9       ↩
+pyftsubset Inter.ttf --unicodes="U+0000-00FF,U+2010-201F,U+20AC,U+2190,U+2192,U+21A9" --layout-features="calt,ccmp,kern,cv07,tnum" --flavor="woff2" --output-file="../assets/fonts/inter.woff2"
+pyftsubset JetBrainsMono.ttf --unicodes="U+0000-007F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono.woff2"
+pyftsubset JetBrainsMono-Italic.ttf --unicodes="U+0000-007F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono-italic.woff2"
