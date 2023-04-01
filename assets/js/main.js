9e8f89f7193804defc8df0324b612fc8f47dcbd8
jsmolka <jsmolka@pm.me>
2023-04-01 17:40:00 +0200

    remove mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ import { createApp, reactive } from 'petite-vue';
 import Audio from './audio';
 
 const store = reactive({
-  menu: false,
   get theme() {
     return window.theme.value;
   },
@@ -12,7 +11,3 @@ const store = reactive({
  });
 
 createApp({ Audio, store }).mount();
-
-for (const type of ['click', 'resize']) {
-  window.addEventListener(type, () => store.menu = false);
-}
