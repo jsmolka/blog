0a843344723d949dc9e6b7d4b9bd1358b5a26d38
jsmolka <jsmolka@pm.me>
2023-05-17 23:38:05 +0200

    back to no trailing slashes

--- scripts/postprocess.js
+++ scripts/postprocess.js
@@ -5,13 +5,39 @@ import { JSDOM } from 'jsdom';
 import { fileURLToPath } from 'url';
 import Prism from '../assets/js/prism.js';
 
+function highlight(document) {
+  let changed = false;
+  if (document.querySelector(':is(code, pre)[class*="language-"]')) {
+    Prism.highlightAllUnder(document);
+    changed = true;
+  }
+  return changed;
+}
+
+function removeTrailingSlashes(document) {
+  let changed = false;
+  for (const link of document.getElementsByTagName('a')) {
+    const href = link.getAttribute('href');
+    const matches = href.match(/^((?:.*smolka\.dev)?\/.*\/)(#.*)?$/);
+    if (matches) {
+      matches.splice(0, 1);  // Remove match
+      matches[0] = matches[0].slice(0, -1);  // Remove trailing slash
+      link.setAttribute('href', matches.join(''));
+      changed = true;
+    }
+  }
+  return changed;
+}
+
 async function postprocess(file) {
   const dom = await JSDOM.fromFile(file);
   const document = dom.window.document;
 
-  if (document.querySelector(':is(code, pre)[class*="language-"]')) {
-    Prism.highlightAllUnder(document);
+  let changed = false;
+  changed |= highlight(document);
+  changed |= removeTrailingSlashes(document);
 
+  if (changed) {
     writeFileSync(file, dom.serialize());
   }
 }
