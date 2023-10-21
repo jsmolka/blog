67a95f4bc05945309ad02fcad7da458338e54bab
jsmolka <jsmolka@pm.me>
2023-10-21 21:19:12 +0200

    make list items clickable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 import { createApp } from "petite-vue";
 import Audio from "./audio";
 
-createApp({ Audio }).mount();
+const href = ({ el, exp }) => {
+  el.style.cursor = "pointer";
+  el.addEventListener("click", () => {
+    location.href = exp;
+  });
+};
+
+const app = createApp({ Audio });
+app.directive("href", href);
+app.mount();
