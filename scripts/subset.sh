1af041c319562cb78a336f6a9296922000210bae
jsmolka <jsmolka@pm.me>
2023-11-21 19:08:59 +0100

    update font

--- scripts/subset.sh
+++ scripts/subset.sh
@@ -9,6 +9,5 @@
 # U+2192       →
 # U+21A9       ↩
 pyftsubset Inter.ttf --unicodes="U+0000-00FF,U+2010-201F,U+20AC,U+2190,U+2192,U+21A9" --layout-features="calt,kern,tnum" --flavor="woff2" --output-file="../assets/fonts/inter.woff2"
-pyftsubset Inter-Italic.ttf --unicodes="U+0000-00FF,U+2010-201F,U+20AC,U+2190,U+2192,U+21A9" --layout-features="calt,kern,tnum" --flavor="woff2" --output-file="../assets/fonts/inter-italic.woff2"
 pyftsubset JetBrainsMono.ttf --unicodes="U+0000-007F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono.woff2"
 pyftsubset JetBrainsMono-Italic.ttf --unicodes="U+0000-007F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono-italic.woff2"
