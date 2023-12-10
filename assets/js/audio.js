16683336a8a6f181bfe14ed8e1e1ab72513da5a8
jsmolka <jsmolka@pm.me>
2023-12-10 08:51:25 +0100

    remove muted

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -2,27 +2,27 @@ import { env } from './utils/env';
 import { math } from './utils/math';
 import { storage } from './utils/storage';
 
-const $template = document.createElement('template');
-$template.innerHTML = /* html */ `
+const template = document.createElement('template');
+template.innerHTML = /* html */ `
   <div class="audio">
-    <button ref="stateButton">
+    <button data-ref="stateButton">
       <svg width="16" height="16" viewBox="5 5 14 14">
-        <path ref="stateButtonPath" fill="currentColor" />
+        <path data-ref="stateButtonPath" fill="currentColor" />
       </svg>
     </button>
-    <div ref="time" class="time">0:00 / 0:00</div>
-    <div ref="progressBar" class="bar">
-      <div ref="progressBarSeeker" class="seeker"></div>
+    <div data-ref="time" class="time">0:00 / 0:00</div>
+    <div data-ref="progressBar" class="bar">
+      <div data-ref="progressBarSeeker" class="seeker"></div>
     </div>
-    <div ref="volume" class="volume">
-      <div ref="volumeBarWrapper" class="volume-bar-wrapper">
-        <div ref="volumeBar" class="bar">
-          <div ref="volumeBarSeeker" class="seeker"></div>
+    <div data-ref="volume" class="volume">
+      <div data-ref="volumeBarWrapper" class="volume-bar-wrapper">
+        <div data-ref="volumeBar" class="bar">
+          <div data-ref="volumeBarSeeker" class="seeker"></div>
         </div>
       </div>
-      <button ref="volumeButton">
+      <button data-ref="volumeButton">
         <svg width="16" height="16" viewBox="3 3 18 18">
-          <path ref="volumeButtonPath" fill="currentColor" />
+          <path data-ref="volumeButtonPath" fill="currentColor" />
         </svg>
       </button>
     </div>
@@ -68,15 +68,12 @@ function initBar(element, opts) {
 }
 
 export function mount(root, src) {
-  const template = $template.content.cloneNode(true);
+  root.appendChild(template.content.cloneNode(true));
 
   const refs = {};
-  for (const element of template.querySelectorAll('[ref]')) {
-    refs[element.getAttribute('ref')] = element;
-    element.removeAttribute('ref');
+  for (const element of root.querySelectorAll('[data-ref]')) {
+    refs[element.getAttribute('data-ref')] = element;
   }
-
-  const audio = new Audio();
 
   const update = () => {
     refs.time.innerHTML = `${formatTime(audio.currentTime)} / ${formatTime(audio.duration)}`;
@@ -86,7 +83,7 @@ export function mount(root, src) {
     );
     refs.volumeButtonPath.setAttribute(
       'd',
-      audio.muted || audio.volume === 0
+      audio.volume === 0
         ? 'M12 4L9.91 6.09L12 8.18M4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.26c-.67.51-1.42.93-2.25 1.17v2.07c1.38-.32 2.63-.95 3.68-1.81L19.73 21L21 19.73l-9-9M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.916 8.916 0 0 0 21 12c0-4.28-3-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71m-2.5 0c0-1.77-1-3.29-2.5-4.03v2.21l2.45 2.45c.05-.2.05-.42.05-.63z'
         : 'M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.84-5 6.7v2.07c4-.91 7-4.49 7-8.77c0-4.28-3-7.86-7-8.77M16.5 12c0-1.77-1-3.29-2.5-4.03V16c1.5-.71 2.5-2.24 2.5-4M3 9v6h4l5 5V4L7 9H3z'
     );
@@ -108,7 +105,6 @@ export function mount(root, src) {
   };
 
   const setVolume = (value) => {
-    audio.muted = false;
     audio.volume = Math.pow(value, 3);
     storage.set('volume', value);
 
@@ -165,9 +161,7 @@ export function mount(root, src) {
     }
   };
 
-  audio.preload = 'metadata';
-  audio.src = src;
-  audio.addEventListener('loadedmetadata', () => {
+  const init = () => {
     update();
 
     setVolume(getVolume());
@@ -197,7 +191,10 @@ export function mount(root, src) {
 
     initProgressBar();
     initVolumeBar();
-  });
+  };
 
-  root.appendChild(template);
+  const audio = new Audio();
+  audio.addEventListener('loadedmetadata', init);
+  audio.preload = 'metadata';
+  audio.src = src;
 }
