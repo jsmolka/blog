e56d4d11d12e95210f52a5dfa8f2772fd2b2c24f
jsmolka <jsmolka@pm.me>
2023-12-11 22:27:58 +0100

    cleanup

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -3,20 +3,11 @@ import { slider } from './utils/slider';
 import { storage } from './utils/storage';
 import { formatSeconds } from './utils/time';
 
-const icon = {
-  play: 'M8 5.14v14l11-7l-11-7z',
-  pause: 'M14 19h4V5h-4M6 19h4V5H6v14z',
-  speaker:
-    'M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.84-5 6.7v2.07c4-.91 7-4.49 7-8.77c0-4.28-3-7.86-7-8.77M16.5 12c0-1.77-1-3.29-2.5-4.03V16c1.5-.71 2.5-2.24 2.5-4M3 9v6h4l5 5V4L7 9H3z',
-  speakerMuted:
-    'M12 4L9.91 6.09L12 8.18M4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.26c-.67.51-1.42.93-2.25 1.17v2.07c1.38-.32 2.63-.95 3.68-1.81L19.73 21L21 19.73l-9-9M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.916 8.916 0 0 0 21 12c0-4.28-3-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71m-2.5 0c0-1.77-1-3.29-2.5-4.03v2.21l2.45 2.45c.05-.2.05-.42.05-.63z',
-};
-
 const template = /* html */ `
   <div class="audio">
     <button data-ref="stateButton">
       <svg width="16" height="16" viewBox="5 5 14 14">
-        <path data-ref="stateButtonPath" fill="currentColor" d="${icon.play}" />
+        <path data-ref="stateButtonPath" fill="currentColor" />
       </svg>
     </button>
     <div data-ref="time" class="time">0:00 / 0:00</div>
@@ -31,7 +22,7 @@ const template = /* html */ `
       </div>
       <button data-ref="volumeButton">
         <svg width="16" height="16" viewBox="3 3 18 18">
-          <path data-ref="volumeButtonPath" fill="currentColor" d="${icon.speaker}" />
+          <path data-ref="volumeButtonPath" fill="currentColor" />
         </svg>
       </button>
     </div>
@@ -72,15 +63,21 @@ export function mount(root, src) {
   };
 
   const update = () => {
-    const currentTime = audio.currentTime || 0;
+    const progress = audio.currentTime || 0;
     const duration = audio.duration || 0;
-    const muted = audio.muted || audio.volume === 0;
-    const progress = duration > 0 ? currentTime / duration : 0;
 
-    refs.time.innerHTML = `${formatSeconds(currentTime)} / ${formatSeconds(duration)}`;
-    refs.stateButtonPath.setAttribute('d', audio.paused ? icon.play : icon.pause);
-    refs.volumeButtonPath.setAttribute('d', muted ? icon.speakerMuted : icon.speaker);
-    refs.progressBarSeeker.style.setProperty('--value', progress);
+    refs.time.innerHTML = `${formatSeconds(progress)} / ${formatSeconds(duration)}`;
+    refs.stateButtonPath.setAttribute(
+      'd',
+      audio.paused ? 'M8 5.14v14l11-7l-11-7z' : 'M14 19h4V5h-4M6 19h4V5H6v14z'
+    );
+    refs.volumeButtonPath.setAttribute(
+      'd',
+      audio.muted || audio.volume === 0
+        ? 'M12 4L9.91 6.09L12 8.18M4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.26c-.67.51-1.42.93-2.25 1.17v2.07c1.38-.32 2.63-.95 3.68-1.81L19.73 21L21 19.73l-9-9M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.916 8.916 0 0 0 21 12c0-4.28-3-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71m-2.5 0c0-1.77-1-3.29-2.5-4.03v2.21l2.45 2.45c.05-.2.05-.42.05-.63z'
+        : 'M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.84-5 6.7v2.07c4-.91 7-4.49 7-8.77c0-4.28-3-7.86-7-8.77M16.5 12c0-1.77-1-3.29-2.5-4.03V16c1.5-.71 2.5-2.24 2.5-4M3 9v6h4l5 5V4L7 9H3z'
+    );
+    refs.progressBarSeeker.style.setProperty('--value', duration > 0 ? progress / duration : 0);
   };
 
   const initProgress = () => {
@@ -108,6 +105,8 @@ export function mount(root, src) {
   };
 
   const initVolume = () => {
+    setVolume(getVolume());
+
     refs.volumeButton.addEventListener('click', () => {
       audio.muted = !audio.muted;
     });
@@ -119,9 +118,9 @@ export function mount(root, src) {
     const active = new Proxy(
       { value: 0 },
       {
-        set(object, property, value) {
-          Reflect.set(...arguments);
+        set(target, key, value) {
           refs.volumeBarWrapper.classList.toggle('active', value > 0);
+          return Reflect.set(target, key, value);
         },
       }
     );
@@ -136,7 +135,7 @@ export function mount(root, src) {
   };
 
   const init = () => {
-    setVolume(getVolume());
+    update();
 
     for (const event of [
       'durationchange',
@@ -152,7 +151,6 @@ export function mount(root, src) {
     ]) {
       audio.addEventListener(event, update);
     }
-    update();
 
     refs.stateButton.addEventListener('click', () => {
       if (audio.paused) {
@@ -170,6 +168,7 @@ export function mount(root, src) {
   audio.addEventListener('loadedmetadata', init);
   audio.preload = 'metadata';
   audio.src = src;
-
   audios.push(audio);
+
+  update();
 }
