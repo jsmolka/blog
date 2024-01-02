1ccd0e72c6a1668958d10a3d7ab38b853d3e18ca
jsmolka <jsmolka@pm.me>
2024-01-02 21:02:35 +0100

    remove custom font workaround

--- scripts/subset.sh
+++ scripts/subset.sh
@@ -9,6 +9,5 @@
 # U+2192       →
 # U+21A9       ↩
 pyftsubset Inter.ttf --unicodes="U+0000-00FF,U+2010-2027,U+20AC,U+2190,U+2192,U+21A9" --layout-features="calt,kern,tnum" --flavor="woff2" --output-file="../assets/fonts/inter.woff2"
-pyftsubset Inter-Bold.ttf --unicodes="U+0030-0039" --layout-features="calt,kern" --flavor="woff2" --output-file="../assets/fonts/inter-bold-numeric.woff2"
 pyftsubset JetBrainsMono.ttf --unicodes="U+0000-007F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono.woff2"
 pyftsubset JetBrainsMono-Italic.ttf --unicodes="U+0000-007F" --layout-features="" --flavor="woff2" --output-file="../assets/fonts/jetbrains-mono-italic.woff2"
