73e1d91606c5df1d12c7e22ed52867856ab248de
jsmolka <jsmolka@pm.me>
2025-12-11 10:10:20 +0100

    add toc

---
+++ assets/js/toc.js
@@ -0,0 +1,18 @@
+const tocLinks = [...document.querySelectorAll('#TableOfContents a[href^="#"]')];
+const headings = tocLinks
+  .map((tocLink) => document.querySelector(`#${tocLink.getAttribute('href').slice(1)}`))
+  .reverse();
+
+function update() {
+  const heading = headings.find(
+    (heading) => heading.getBoundingClientRect().top <= 0.2 * window.innerHeight
+  );
+
+  for (const tocLink of tocLinks) {
+    const isActive = heading != null && tocLink.getAttribute('href').slice(1) === heading.id;
+    tocLink.classList.toggle('active', isActive);
+  }
+}
+
+window.addEventListener('scroll', update, { passive: true });
+window.addEventListener('hashchange', update, { passive: true });
