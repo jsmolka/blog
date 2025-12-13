cd964f722dd17eea4a23f4e9c7449bda5c080fd1
jsmolka <jsmolka@pm.me>
2025-12-13 12:37:30 +0100

    improve image processing

--- scripts/process-images.sh
+++ scripts/process-images.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# sudo apt-get install advancecomp imagemagick libjpeg-progs zopfli
-
-shopt -s globstar nullglob
+# sudo apt-get install curl imagemagick libjpeg-progs
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# cargo install oxipng
 
 dir="$1"
-if [[ -z "$dir" || ! -d "$dir" ]]; then
+if [[ -z "$dir" ]]; then
   echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+if [[ ! -d "$dir" ]]; then
+  echo "$dir does not exist"
   exit 1
 fi
 
@@ -14,41 +19,31 @@ w=1280
 h=1280
 q=80
 
-for img in "$dir"/**/*.{jpg,jpeg}; do
+shopt -s globstar nullglob
+
+for img in "$dir"/**/*.{jpg,jpeg,png}; do
   echo "$img"
 
-  img_w=$(identify -format "%w" "$img")
-  img_h=$(identify -format "%h" "$img")
-  img_q=$(identify -format "%Q" "$img")
-  if (( img_w <= w && img_h <= h && img_q <= q )); then
-    continue
-  fi
+  if [[ "$img" =~ \.(jpg|jpeg)$ ]]; then
+    img_w=$(identify -quiet -format "%w" "$img")
+    img_h=$(identify -quiet -format "%h" "$img")
+    img_q=$(identify -quiet -format "%Q" "$img")
 
-  tmp=$(mktemp)
-  convert "$img" -resize "${w}x${h}>" ppm:- | cjpeg -quality $(( img_q < q ? img_q : q )) -progressive -optimize > "$tmp"
-
-  if [[ -f "$tmp" ]]; then
-    img_size=$(stat -c%s "$img")
-    tmp_size=$(stat -c%s "$tmp")
-    if (( tmp_size < img_size )); then
-      mv -f "$tmp" "$img"
-    else
-      rm "$tmp"
+    if (( img_w <= w && img_h <= h && img_q <= q )); then
+      continue
     fi
+
+    tmp=$(mktemp)
+    convert "$img" -resize "${w}x${h}>" ppm:- | cjpeg -quality $(( img_q < q ? img_q : q )) -progressive -optimize > "$tmp"
+  else
+    tmp=$(mktemp)
+    convert "$img" -resize "${w}x${h}>" png:- | oxipng --opt max --zopfli --fast -s -q - --out "$tmp"
   fi
-done
-
-for img in "$dir"/**/*.png; do
-  echo "$img"
-
-  tmp=$(mktemp)
-  convert "$img" -resize "${w}x${h}>" "$tmp"
-  zopflipng -m -y "$tmp" "$tmp" >/dev/null
-  advpng -z4 "$tmp" >/dev/null
 
   if [[ -f "$tmp" ]]; then
     img_size=$(stat -c%s "$img")
     tmp_size=$(stat -c%s "$tmp")
+
     if (( tmp_size < img_size )); then
       mv -f "$tmp" "$img"
     else
